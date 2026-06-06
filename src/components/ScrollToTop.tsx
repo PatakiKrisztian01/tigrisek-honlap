5a4df48001b7367311c033612ea13f07ffc7c7d1
PatakiKrisztian01 <patakikrisztian.dez@gmail.com>
2026-06-06 19:04:46 +0200

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,11 +2,20 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'auto' });
-  }, [pathname]);
+    // Ha van #azonosító az URL-ben, ne ugorjon a tetejére, hanem keresse meg az elemet
+    if (hash) {
+      const element = document.getElementById(hash.substring(1));
+      if (element) {
+        setTimeout(() => element.scrollIntoView({ behavior: 'smooth' }), 100);
+      }
+    } else {
+      // Egyébként mehet a tetejére
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
 
   return null;
 }
