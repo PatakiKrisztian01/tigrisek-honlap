02d3e433506ececcb4c033ba9c19307dfe0b90ac
PatakiKrisztian01 <patakikrisztian.dez@gmail.com>
2026-06-08 20:24:42 +0200

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -202,15 +202,15 @@ export default function Home() {
           {/* 11. Sor: Közeledünk a csúcshoz, a TIGERS SE fixen egyben, középen zár */}
           <div className="flex justify-center items-center py-1.5 w-full whitespace-nowrap">
             <span className="text-white inline-block whitespace-nowrap mr-20">ERŐS TEST ERŐS LÉLEK EDZŐTERMI KÖZÖSSÉG CSALÁDIAS HANGULAT</span>
-            <span className="text-red-500 font-black bg-red-950/30 px-2 py-0.5 rounded inline-block whitespace-nowrap">CÉLOK TIGERS SE</span>
+            <span className="text-red-500 font-black bg-red-950/30 px-2 py-0.5 rounded inline-block whitespace-nowrap">SPORTEGYESLÜLET</span>
             <span className="text-white inline-block whitespace-nowrap ml-20">JÓ HANGULAT PONTOSSÁG RÚGÁSOK ÜTÉSEK PAZS EDZÉS ZSÁKOLÁS</span>
           </div>
 
           {/* 12. Sor: A szív legalsó csúcsa (A weboldal címe tiszta fehér/narancs fókuszpontként zárja le a formát) */}
           <div className="flex justify-center items-center py-1.5 w-full whitespace-nowrap">
-            <span className="text-gray-500 opacity-20 inline-block whitespace-nowrap mr-12">ERŐNLÉT RUGALMASSÁG GYORSASÁG  KARATE REAKCIÓIDŐ TAGDÍJ PRÓBA VÁRUNK RÁD ALAPÍTVA 2002</span>
-            <span className="text-white font-black bg-neon-orange/20 px-4 py-1 border border-neon-orange/40 rounded text-xs tracking-widest inline-block whitespace-nowrap shadow-[0_0_15px_rgba(255,94,0,0.2)]">WWW.TIGRISEK.HU</span>
-            <span className="text-gray-500 opacity-20 inline-block whitespace-nowrap ml-12">BUDAPESTI KICKBOX KERÜLETI TAEKWONDO HAVANNA LAKÓTELEPI SPORT GYEREKEKNEK FELNŐTTEKNEK</span>
+            <span className="text-gray-500 opacity-20 inline-block whitespace-nowrap mr-12">ERŐNLÉT RUGALMASSÁG GYORSASÁG  KARATE REAKCIÓIDŐ TAGDÍJ PRÓBA</span>
+            <span className="text-red-500 font-black bg-red-950/30 px-2 py-0.5 rounded inline-block whitespace-nowrap">TIGERS</span>
+            <span className="text-gray-500 opacity-20 inline-block whitespace-nowrap ml-12">BUDAPESTI KICKBOX KERÜLETI TAEKWONDO HAVANNA LAKÓTELEPI SPORT</span>
           </div>
 
         </div>
