d1a87e241452aebb6f4c124cd8fc62a2d2a23303
PatakiKrisztian01 <patakikrisztian.dez@gmail.com>
2026-06-10 14:06:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,14 @@ export default {
         'neon-orange': '#FF5F1F',
         'deep-black': '#0a0a0a',
       },
-      // --- IDE KERÜLTEK BE A FUTÓ HÍRFOLYAM BEÁLLÍTÁSAI ---
       animation: {
-        // 25 másodperces sebesség, egyenletes mozgás (linear), végtelenítve (infinite)
+        // 25 másodperces folyamatos mozgás
         marquee: 'marquee 25s linear infinite',
       },
       keyframes: {
         marquee: {
-          // Mivel balról jobbra szeretnéd, a negatív -50%-ról (balról) úsztatjuk be 0%-ra (jobbra)
-          '0%': { transform: 'translateX(-50%)' },
-          '100%': { transform: 'translateX(0%)' },
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
         },
       },
     },
